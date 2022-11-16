65264b4f1d4bbf87a40b44e4cb9e60f70d0ce0f1
Milosz Gembicki <mg1313@live.mdx.ac.uk>
2022-11-16 18:46:12 +0000

    Added products.js and edited html + css files

---
+++ products.js
@@ -0,0 +1,52 @@
+let products = [
+    {
+        "id": 1001,
+        "imgALT": "Math img",
+        "imgSRC": "math.png",
+        "title": "Subject: Math",
+        "location": "Location: London",
+        "price": 10.00,
+        "spaces": 10,
+        "rating": 3
+    },
+    {
+        "id": 1002,
+        "imgALT": "Math img",
+        "imgSRC": "math.png",
+        "title": "Subject: Math",
+        "location": "Location: London",
+        "price": 11.00,
+        "spaces": 16,
+        "rating": 2
+    },
+    {
+        "id": 1003,
+        "imgALT": "Math img",
+        "imgSRC": "math.png",
+        "title": "Subject: Math",
+        "location": "Location: London",
+        "price": 25.99,
+        "spaces": 6,
+        "rating": 5
+    },
+    {
+        "id": 1004,
+        "imgALT": "Math img",
+        "imgSRC": "math.png",
+        "title": "Subject: Math",
+        "location": "Location: London",
+        "price": 5.99,
+        "spaces": 11,
+        "rating": 4
+    },
+    {
+        "id": 1005,
+        "imgALT": "Math img",
+        "imgSRC": "math.png",
+        "title": "Subject: Math",
+        "location": "Location: London",
+        "price": 8.99,
+        "spaces": 15,
+        "rating": 5
+    },
+]
