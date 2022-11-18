6600a0a0b0fbb6ddd9e2e277d7b444a05f7495fe
Milosz Gembicki <mg1313@live.mdx.ac.uk>
2022-11-18 01:38:42 +0000

    Added search function, with updated products list

--- products.js
+++ products.js
@@ -2,51 +2,101 @@ let products = [
     {
         "id": 1001,
         "imgALT": "Math img",
-        "imgSRC": "math.png",
-        "title": "A",
-        "location": "Tatry",
+        "imgSRC": "class_icons/math.png",
+        "title": "Math",
+        "location": "Glasgow",
         "price": 10.00,
-        "spaces": 10,
-        "rating": 3
+        "spaces": 5,
+        "spacesLeft": 5
     },
     {
         "id": 1002,
-        "imgALT": "Math img",
-        "imgSRC": "math.png",
-        "title": "G",
-        "location": "Abry",
+        "imgALT": "Music img",
+        "imgSRC": "class_icons/music.png",
+        "title": "Music",
+        "location": "Luton",
         "price": 11.00,
-        "spaces": 16,
-        "rating": 2
+        "spaces": 5,
+        "spacesLeft": 5
     },
     {
         "id": 1003,
         "imgALT": "Math img",
-        "imgSRC": "math.png",
-        "title": "L",
+        "imgSRC": "class_icons/math.png",
+        "title": "Math",
         "location": "London",
         "price": 25.99,
-        "spaces": 6,
-        "rating": 5
+        "spaces": 5,
+        "spacesLeft": 5
     },
     {
         "id": 1004,
         "imgALT": "Math img",
-        "imgSRC": "math.png",
-        "title": "B",
-        "location": "Warsaw",
+        "imgSRC": "class_icons/math.png",
+        "title": "Math",
+        "location": "Liverpool",
         "price": 5.99,
-        "spaces": 11,
-        "rating": 4
+        "spaces": 5,
+        "spacesLeft": 5
     },
     {
         "id": 1005,
-        "imgALT": "Math img",
-        "imgSRC": "math.png",
-        "title": "D",
+        "imgALT": "Arts img",
+        "imgSRC": "class_icons/arts.png",
+        "title": "Arts",
+        "location": "Liverpool",
+        "price": 8.99,
+        "spaces": 5,
+        "spacesLeft": 5
+    },
+    {
+        "id": 1006,
+        "imgALT": "IT img",
+        "imgSRC": "class_icons/it.png",
+        "title": "IT",
+        "location": "Manchester",
+        "price": 8.99,
+        "spaces": 5,
+        "spacesLeft": 5
+    },
+    {
+        "id": 1007,
+        "imgALT": "History img",
+        "imgSRC": "class_icons/history.png",
+        "title": "History",
+        "location": "London",
+        "price": 8.99,
+        "spaces": 5,
+        "spacesLeft": 5
+    },
+    {
+        "id": 1008,
+        "imgALT": "English img",
+        "imgSRC": "class_icons/english.png",
+        "title": "English",
+        "location": "London",
+        "price": 8.99,
+        "spaces": 5,
+        "spacesLeft": 5
+    },
+    {
+        "id": 1009,
+        "imgALT": "Science img",
+        "imgSRC": "class_icons/science.png",
+        "title": "Science",
+        "location": "Sussex",
+        "price": 8.99,
+        "spaces": 5,
+        "spacesLeft": 5
+    },
+    {
+        "id": 1010,
+        "imgALT": "Science img",
+        "imgSRC": "class_icons/science.png",
+        "title": "Science",
         "location": "Glasgow",
         "price": 8.99,
-        "spaces": 15,
-        "rating": 5
+        "spaces": 5,
+        "spacesLeft": 5
     },
 ]
